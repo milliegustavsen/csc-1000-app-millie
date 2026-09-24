68fd6184284777de22c01a5414c6286da3b308f4
Millie Gustavsen <gustavse@calpoly.edu>
2026-09-24 13:48:49 -0700

    big ie4 commit

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,121 +1,83 @@
 import express from "express";
 import cors from "cors";
+import userServices from "./services/user-service.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const { MONGO_CONNECTION_STRING } = process.env;
+
+mongoose.set("debug", true);
+mongoose
+  .connect(MONGO_CONNECTION_STRING + "users") // connect to Db "users"
+  .catch((error) => console.log(error));
 
 const app = express();
 const port = 8000;
 
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor",
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ],
-};
-
 app.use(cors());
 app.use(express.json());
 
-const findUserByName = (name) => {
-  return users["users_list"].filter((user) => user["name"] === name);
-};
-
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
 app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
-});
+  userServices
+    .findUserById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send("Resource not found.");
+      }
 
-const addUser = (user) => {
-  // made id into a string
-  user.id = String(Math.floor(Math.random() * 100000));
-  users["users_list"].push(user);
-  return user;
-};
+      res.json(user);
+    })
+    .catch((error) => {
+      res.status(500).send("Database error."); // used ChatGPT to find what error to send
+    });
+});
 
 app.post("/users", (req, res) => {
-  const userToAdd = req.body;
-  addUser(userToAdd);
-  res.status(201).json(userToAdd);
-  //res.status(201).send("User Added!")
-  //res.send();
+  userServices
+    .addUser(req.body)
+    .then((newUser) => {
+      res.status(201).json(newUser);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("Database error.");
+    });
 });
-
-const deleteUserById = (id) => {
-  const index = users.users_list.findIndex((user) => user.id === id);
-  if (index === -1) return undefined;
-  return users.users_list.splice(index, 1)[0];
-};
 
 app.delete("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = deleteUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.status(204).send();
-  }
-});
+  userServices
+    .removeUser(req.params.id)
+    .then((deletedUser) => {
+      if (!deletedUser) {
+        return res.status(404).send("Resource not found.");
+      }
 
-const findUserByNameJob = (name, job) => {
-  return users.users_list.find(
-    (user) => user.name === name && user.job === job
-  );
-};
+      res.status(204).send();
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("Database error.");
+    });
+});
 
 app.get("/users", (req, res) => {
   const { name, job } = req.query;
 
-  if (name && job) {
-    const user = findUserByNameJob(name, job);
-
-    if (!user) return res.status(404).send("Resource not found.");
-
-    return res.send({ users_list: [user] });
-  }
-
-  if (name) {
-    return res.send({ users_list: findUserByName(name) });
-  }
-
-  res.send(users);
-});
-
-app.get("/", (req, res) => {
-  res.send(users);
+  userServices
+    .getUsers(name, job)
+    .then((users) => {
+      res.json({ users_list: users });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("Database error.");
+    });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-// deleted this from json but might need it? "type": "commonjs",
 // to debug: $env:DEBUG='express:router'; npx nodemon backend.js
